10b1d95a5216e07119bdbeb469a28a719198404f
SourabhCodeTech <vandanagautam540@gmail.com>
2022-05-05 21:33:41 +0530

    first commit

---
+++ Templates/Snake_Template/JS/responsive.js
@@ -0,0 +1,74 @@
+// let toggle = document.getElementById('toggle');
+
+// toggle.addEventListener('click', () => {
+//   let hidden = document.getElementById('hidden').classList.toggle('hidden');
+// });
+
+
+let h1textani = document.querySelector('.h1textani')
+
+// if (h1textani.value != 0) {
+//   setTimeout(() => {
+//     h1textani.innerHTML = "We"
+//   }, 100);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Wel"
+//   }, 200);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welc"
+//   }, 300);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welco"
+//   }, 300);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcom"
+//   }, 400);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome"
+//   }, 500);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome t"
+//   }, 600);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to"
+//   }, 700);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to S"
+//   }, 800);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Sn"
+//   }, 900);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Sna"
+//   }, 1000);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snak"
+//   }, 1100);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake "
+//   }, 1200);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake T"
+//   }, 1300);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Te"
+//   }, 1400);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Tem"
+//   }, 1500);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Temp"
+//   }, 1600);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Templ"
+//   }, 1700);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Templa"
+//   }, 1800);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Templat"
+//   }, 1900);
+//   setTimeout(() => {
+//     h1textani.innerHTML = "Welcome to Snake Template"
+//   }, 2000);
+// }
